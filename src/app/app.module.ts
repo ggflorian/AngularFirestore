f7370aa496e60c18439f3c738cb869df641ab478
Florian <ggflorian@gmx.com>
2020-04-11 16:13:47 +0000

    new commit; check cust

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,22 +16,35 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { ToastrModule } from 'ngx-toastr'
 
 import { environment } from 'src/environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database'; // without public_api?
+import { AppRoutingModule } from './app-routing.module';
+import { CustomerDetailsComponent } from './customers/customer-details/customer-details.component';
+import { CustomersListComponent } from './customers/customers-list/customers-list.component';
+import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
+import { CustomerService } from './customers/customer.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+
     BooksComponent,
     BookComponent,
-    BookListComponent
+    BookListComponent,
+
+    CustomerDetailsComponent,
+    CustomersListComponent,
+    CreateCustomerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
+    AngularFireDatabaseModule,
     ToastrModule.forRoot()
   ],
-  providers: [BookService],
+  providers: [BookService, CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/customers/create-customer/create-customer.component.ts
+++ src/app/customers/create-customer/create-customer.component.ts
@@ -17,6 +17,7 @@ export class CreateCustomerComponent implements OnInit {
   constructor(private customerService: CustomerService) { }
 
   ngOnInit() {
+    alert('create cust 1');
   }
 
   newCustomer(): void {
