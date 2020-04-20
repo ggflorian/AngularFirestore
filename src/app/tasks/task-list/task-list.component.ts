9921b7b9d2ba2967d635ece9a98fe5025ded14aa
florian <ggflorian@gmx.com>
2020-04-20 10:27:11 +0000

    added task3

--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -17,66 +17,70 @@ export class TaskListComponent implements OnInit {
   tasks: Observable<any[]>;
 
   myTask: string;
-editMode: boolean = false;
-taskToEdit: any = {};
+  editMode: boolean = false;
+  taskToEdit: any = {};
 
   constructor(private db: AngularFirestore, private taskService: TaskService) { }
 
   ngOnInit(): void {
     this.tasks = this.db
-.collection(appConfig.endpoint)
-.snapshotChanges().pipe(
-map(actions => {
-   return actions.map(a => {
+    .collection(appConfig.endpoint)
+    .snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
 
-     //Get document data
-     const taskData = a.payload.doc.data() as Task;
-     
-     //Get document id
-     const id = a.payload.doc.id;
-     
-     //Use spread operator to add the id to the document data
-     return { id, ...taskData };
-   });
-}));
-}
+          //Get document data
+          const taskData = a.payload.doc.data() as Task;
+          
+          //Get document id
+          const id = a.payload.doc.id;
+          
+          //Use spread operator to add the id to the document data
+          return { id, ...taskData };
+        });
+      }));
+  }
 
+  edit(task) {
+    //Set taskToEdit and editMode
+    this.taskToEdit = task;
+    this.editMode = true;
 
-edit(task) {
-  console.log(task);
-  //Set taskToEdit and editMode
-  this.taskToEdit = task;
-  this.editMode = true;
-  //Set form value
-  this.myTask = task.description;
-} //edit
+    //Set form value
+    this.myTask = task.description;
+  }
 
-saveTask() {
-   if (this.myTask !== null) {
-      //Get the input value
-      let task = {
-         description: this.myTask
-      };
-      if (!this.editMode) {
-         console.log(task);
-         this.taskService.addTask(task);
-      } else {
-         //Get the task id
-         let taskId = this.taskToEdit.id;
-         //update the task
-         this.taskService.updateTask(taskId, task);
-      }
-      //set edit mode to false and clear form
-      this.editMode = false;
-      this.myTask = "";
-   }
-} //saveTask
+  saveTask() {
+    if (this.myTask !== null) {
+        //Get the input value
+        let task = {
+          description: this.myTask
+        };
 
-deleteTask(task) {
-   //Get the task id
-   let taskId = task.id;
-   //delete the task
-   this.taskService.deleteTask(taskId);
-} //deleteTask
+        if (!this.editMode) {
+          this.taskService.addTask(task);
+        } else {
+
+          //Get the task id
+          let taskId = this.taskToEdit.id;
+
+          //update the task
+          this.taskService.updateTask(taskId, task);
+        }
+
+        //set edit mode to false and clear form
+        this.editMode = false;
+        this.myTask = "";
+    }
+  }
+
+  deleteTask(task) {
+  
+    //Get the task id
+    let taskId = task.id;
+
+    //delete the task
+    this.taskService.deleteTask(taskId);
+  }
 
 }
